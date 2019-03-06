0206818a905698ca486c8532a692f0cda46f1178
ortizk <ortizk@live.com>
2019-03-06 13:03:29 -0800

    refactored

--- removeList/removeList.js
+++ removeList/removeList.js
@@ -3,21 +3,21 @@
 // If a value is present in b, all of its occurrences must be removed from the other:
 
 
-// const array_diff = (arr, remove) => {
-// 	var filtered = arr.filter(item => !remove.includes(item))
-// 	return filtered
-// }
-// console.log(array_diff([1,2,2,2,3],[2])) // == [1,3]
-// console.log(array_diff([1,2],[1])) // == [2]
+const array_diff = (arr, remove) => {
+	var filtered = arr.filter(item => !remove.includes(item))
+	return filtered
+}
+console.log(array_diff([1,2,2,2,3],[2])) // == [1,3]
+console.log(array_diff([1,2],[1])) // == [2]
 
 
 // take out all of the "the" 's in this sentence
 
-const the = (str, word) => {
-	str = str.split(' ')
-	var noThe = str.filter(item => !word.includes(item))
-	return noThe.join(" ")
-}
+// const the = (str, word) => {
+// 	str = str.split(' ')
+// 	var noThe = str.filter(item => word !== item)
+// 	return noThe.join(" ")
+// }
 
 
-console.log(the("the time when there was the best french onion soup was the best time ever", "the"))
+// console.log(the("the time when there was the best french onion soup was the best time ever", "the"))
