a168124cd06edf88f0e5feb5e063df28cde5843d
ortizk <ortizk@live.com>
2019-03-13 18:42:55 -0700

    solved sortedRotated

---
+++ sortedRotated/sortedRotated.js
@@ -0,0 +1,17 @@
+// check if a given array is sorted even when rotated
+
+// splice at lowest index and concat both arrays together and see if sorted 
+
+const sortedRotated = (arr) => {
+	let lowest = [arr.indexOf(Math.min(...arr))];
+	let unrotated = [...arr.slice(lowest[1]), ...arr.slice(0, lowest[1])]
+	for(let i = 0;i < unrotated.length;i++){
+		if(unrotated[i] > unrotated[i+1]){
+			return false
+		}
+	}
+	return true
+	
+}
+
+console.log(sortedRotated([3, 4, 7, 5, 1, 2]))
